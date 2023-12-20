aedbbf08924ea69bfc67a60f1940e93e5b3d7402
Martynas Prusevicius <mprusevicius@gmail.com>
2023-12-20 17:26:08 +0000

    WIP Add enemy

---
+++ Bad guy.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+var SPEED = 300
+
+func _physics_process(delta):
+	var player_character = get_parent().get_node("Player")
+	var direction = player_character.position - position
+	var normalised_direction = direction.normalized()
+	velocity = normalised_direction * SPEED
+	move_and_slide()
