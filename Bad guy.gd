7644380739f5f8acb56aea5ebbf1d93d1f9bf8fa
Mukesh Patel <mpatel125@outlook.com>
2023-12-20 17:59:52 +0000

    WIP Trying to fix bad guy colliders

--- Bad guy.gd
+++ Bad guy.gd
@@ -4,8 +4,6 @@ var SPEED = 200
 func _physics_process(delta):
 	var player_character = get_parent().get_node("Player")
 	var direction = player_character.position - position
-	print_debug(player_character.position, position)
 	var normalised_direction = direction.normalized()
 	velocity = normalised_direction * SPEED
-	print_debug(velocity)
 	move_and_slide()
--- Bullet.gd
+++ Bullet.gd
@@ -6,7 +6,14 @@ func _physics_process(delta):
 	position += transform.x * SPEED * delta
 
 #func _on_Bullet_body_entered(body):
+	#print_debug("ouchies")
 	#if body.is_in_group("mobs"):
 		#body.queue_free()
 	#queue_free()
 
+
+func _on_bad_guy_area_2d_body_entered(body):
+	print_debug("ouchies")
+	#if body.is_in_group("mobs"):
+		#body.queue_free()
+	queue_free()
