a678868ca00d437d6e7134f7893b95c21cc07d89
Martynas Prusevicius <mprusevicius@gmail.com>
2023-12-22 16:29:30 +0000

    Add scaling when enemy hit

--- Bullet.gd
+++ Bullet.gd
@@ -7,6 +7,8 @@ func _physics_process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("mobs"):
-		body.queue_free()
+		print(body)
+		print(body.scale)
+		body.scale = Vector2(1, 1)
 	if not body.is_in_group("player"):
 		queue_free()
